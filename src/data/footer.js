0e5b464d062e3a0aaedd5189b240f465809c4c93
Sergei <sergei@odincov.net>
2015-01-27 11:38:38 +0000

    styling up the footer lists

--- src/data/footer.js
+++ src/data/footer.js
@@ -2,7 +2,7 @@ var data = {}
 
 data['links-title'] = 'Ссылки на материалы'
 data['credits-title'] = '«История исследования Плутона»'
-data['credits-sub-title'] = 'Проект сайта «Большая Вселенная»'
+data['credits-sub-title'] = 'Проект сайта <span>«Большая Вселенная»</span>'
 data['references-title'] = 'Использованные материалы'
 data['gratitude'] = 'Благодарность'
 data['copy-rights'] = '© Большая Вселенная 2011 — 2015 гг.'
