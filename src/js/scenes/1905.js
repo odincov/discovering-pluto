3f53bb55f3b72168b56ce4060e66ac037f4a5ed1
Sergei <sergei@odincov.net>
2015-02-10 16:34:53 +0000

    progressing slides

--- src/js/scenes/1905.js
+++ src/js/scenes/1905.js
@@ -1,13 +1,15 @@
 var ScrollScene = require('../../vendor/ScrollMagic/js/jquery.scrollmagic').Scene
 
+var timeline = new TimelineMax()
+timeline
+  .from('.slide__bg--1905', 1, { opacity: 0, scale: 1.25, ease: Expo.easeOut })
+  .from('.slide--1905 h2', 0.5, { opacity: 0, x: 50 }, "-=0.5")
+  .from('.slide--1905 p', 0.5, { opacity: 0, x: 50 })
+
 var scene = new ScrollScene({ 
-  triggerElement : '.slide--1905',
+  triggerElement: '.slide--1905',
+  // duration: 500
 })
-.offset(200)
-.setTween(TweenMax.from('.slide__bg--1905', 1, { 
-  opacity: 0,
-  scale: 1.25,
-  ease: Expo.easeOut
-}))
+.setTween(timeline)
 
 module.exports = scene
--- src/js/scenes/1915.js
+++ src/js/scenes/1915.js
@@ -1,12 +1,15 @@
 var ScrollScene = require('../../vendor/ScrollMagic/js/jquery.scrollmagic').Scene
 
+var timeline = new TimelineMax()
+timeline
+  .from('.slide__bg--1915', 1, { opacity: 0, ease: Expo.easeOut })
+  .from('.slide--1915 h2', 0.5, { opacity: 0, y: 50 }, "-=0.5")
+  .from('.slide--1915 p', 0.5, { opacity: 0, y: 50 })
+
 var scene = new ScrollScene({ 
   triggerElement : '.slide--1915',
 })
-.offset(300)
-.setTween(TweenMax.from('.slide__bg--1915', 1.5, { 
-  opacity: 0,
-  ease: Expo.easeOut
-}))
+// .offset(300)
+.setTween(timeline)
 
 module.exports = scene
