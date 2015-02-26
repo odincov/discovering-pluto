ba288fd8eeb9d416fbd100c76e833d9398165c3c
Sergei <sergei@odincov.net>
2015-02-26 12:32:45 +0000

    dist refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -3,57 +3,69 @@ var gulp = require('gulp')
   , reload = browserSync.reload
   , webpack = require('gulp-webpack')
   , jade = require('gulp-jade')
-  , debug = require('gulp-debug')
-  , autoprefixee = require('gulp-autoprefixer')
+  , autoprefixer = require('gulp-autoprefixer')
   , uglify = require('gulp-uglify')
   , concat = require('gulp-concat')
   , stylus = require('gulp-stylus')
   , koutoSwiss = require('kouto-swiss')
   , path = require('path')
   , iconify = require('gulp-iconify')
+  , gutil = require('gulp-util')
+  , minifyCSS = require('gulp-minify-css')
+  , defaultLang = 'ru'
 
 gulp.task('stylus', function () {
+  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/stylus/main.styl')
     .pipe(stylus({ use: [ koutoSwiss() ] }))
-    .pipe(gulp.dest('./dist/css/'))
+    .pipe(gutil.env.type === 'production' ? minifyCSS() : gutil.noop())
+    .pipe(gulp.dest('./dist/'+lang+'/css/'))
     .pipe(reload({ stream : true }))
 })
 
 gulp.task('jade', function() {
+  var lang = gutil.env.lang || defaultLang
+  var dataPath = './src/data/'+lang
   return gulp.src('./src/views/pages/*.jade')
     .pipe(jade({ 
       locals: { 
-        intro : require('./src/data/intro.js'),
-        slides : require('./src/data/slides.js'),
-        subscribeView : require('./src/data/subscribe.js'),
-        footer : require('./src/data/footer.js'),
-        links : require('./src/data/links.js'),
-        credits : require('./src/data/credits.js'),
-        references : require('./src/data/references.js'),
-        gratitudes : require('./src/data/gratitudes.js')
+        intro : require(dataPath+'/intro.js'),
+        slides : require(dataPath+'/slides.js'),
+        subscribeView : require(dataPath+'/subscribe.js'),
+        footer : require(dataPath+'/footer.js'),
+        links : require(dataPath+'/links.js'),
+        credits : require(dataPath+'/credits.js'),
+        references : require(dataPath+'/references.js'),
+        gratitudes : require(dataPath+'/gratitudes.js')
       }
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'+lang+'/'))
 })
 
 gulp.task('images', function() {
+  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/images/*.*')
-    .pipe(gulp.dest('./dist/images/'))
+    .pipe(gulp.dest('./dist/'+lang+'/images/'))
 })
 
 gulp.task('videos', function() {
+  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/videos/*.*')
-    .pipe(gulp.dest('./dist/videos/'))
+    .pipe(gulp.dest('./dist/'+lang+'/videos/'))
 })
 
 gulp.task('iconify', function() {
+  var lang = gutil.env.lang || defaultLang
   return iconify({
     src: './src/icons/*.svg',
-    cssOutput: './dist/css'
+    cssOutput: './dist/'+lang+'/css/',
+    pngOutput: './dist/'+lang+'/images/icons/',
+    scssOutput: './dist/scss/'
   })
 })
 
 gulp.task('js', function(){
+  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/js/index.js')
     .pipe(webpack({ 
       output : { filename : 'index.js' },
@@ -68,20 +80,22 @@ gulp.task('js', function(){
         }
       }
     }))
-    .pipe(gulp.dest('./dist/js/'))
+    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(gulp.dest('./dist/'+lang+'/js/'))
 })
 
 gulp.task('browser-sync', function() {
+  var lang = gutil.env.lang || defaultLang
   browserSync({
-    server: { baseDir: './dist/' },
+    server: { baseDir: './dist/'+lang+'/' },
     port: 7200,
     open: false
   })
 })
 
-gulp.task('build', ['stylus','jade','js','images','videos'])
+gulp.task('build', ['stylus','jade','js','iconify','images','videos'])
 
-gulp.task('go', ['stylus','jade','js','images','videos','browser-sync'], function() {
+gulp.task('go', ['build','browser-sync'], function() {
   gulp.watch('./src/stylus/**/*.styl', ['stylus'])
   gulp.watch('./src/views/**/*.jade', ['jade', reload])
   gulp.watch('./src/js/**/*.js', ['js', reload])
---
+++ src/data/ru/footer.js
@@ -0,0 +1,10 @@
+var data = {}
+
+data['links-title'] = 'Ссылки на материалы'
+data['credits-title'] = '«История исследования Плутона»'
+data['credits-sub-title'] = 'Проект сайта <span>«Большая Вселенная»</span>'
+data['references-title'] = 'Использованные материалы'
+data['gratitude'] = 'Благодарность'
+data['copy-rights'] = '© Большая Вселенная 2011 — 2015 гг.'
+
+module.exports = data
