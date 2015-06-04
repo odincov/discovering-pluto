3c64805b694519df9a13b411e4c899da6ef98220
Sergei Sapoznikov <sergei.sapoznikov@gmail.com>
2015-06-04 22:37:18 +0100

    Change gulp build to generate all language versions at once

--- gulpfile.js
+++ gulpfile.js
@@ -12,64 +12,64 @@ var gulp = require('gulp')
   , iconify = require('gulp-iconify')
   , gutil = require('gulp-util')
   , minifyCSS = require('gulp-minify-css')
-  , ftp = require('gulp-ftp')
-  , defaultLang = 'ru'
   , gulpif = require('gulp-if')
   , gzip = require('gulp-gzip')
   , size = require('gulp-size')
+  , merge = require('merge-stream')
 
 gulp.task('stylus', function () {
-  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/stylus/main.styl')
     .pipe(stylus({ use: [ koutoSwiss() ] }))
     .pipe(gutil.env.type === 'production' ? minifyCSS() : gutil.noop())
-    .pipe(gulp.dest('./dist/'+lang+'/css/'))
+    .pipe(gulp.dest('./dist/css/'))
     .pipe(reload({ stream : true }))
 })
 
 gulp.task('jade', function() {
-  var lang = gutil.env.lang || defaultLang
-  var dataPath = './src/data/'+lang
-  return gulp.src('./src/views/pages/*.jade')
-    .pipe(jade({ 
-      locals: { 
-        intro : require(dataPath+'/intro.js'),
-        slides : require(dataPath+'/slides.js'),
-        subscribeView : require(dataPath+'/subscribe.js'),
-        footer : require(dataPath+'/footer.js'),
-        links : require(dataPath+'/links.js'),
-        credits : require(dataPath+'/credits.js'),
-        references : require(dataPath+'/references.js'),
-        gratitudes : require(dataPath+'/gratitudes.js')
-      }
-    }))
-    .pipe(gulp.dest('./dist/'+lang+'/'))
+  var langs = ['ru','en'];
+
+  var tasks = langs.map(function (lang) {
+    var dataPath = './src/data/'+lang
+    return gulp.src('./src/views/pages/*.jade')
+      .pipe(jade({ 
+        locals: { 
+          intro : require(dataPath+'/intro.js'),
+          slides : require(dataPath+'/slides.js'),
+          subscribeView : require(dataPath+'/subscribe.js'),
+          footer : require(dataPath+'/footer.js'),
+          links : require(dataPath+'/links.js'),
+          credits : require(dataPath+'/credits.js'),
+          references : require(dataPath+'/references.js'),
+          gratitudes : require(dataPath+'/gratitudes.js')
+        }
+      }))
+      .pipe(gulp.dest('./dist/'+lang+'/'))
+  });
+
+  return merge(tasks);
+  
 })
 
 gulp.task('images', function() {
-  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/images/*.*')
-    .pipe(gulp.dest('./dist/'+lang+'/images/'))
+    .pipe(gulp.dest('./dist/images/'))
 })
 
 gulp.task('videos', function() {
-  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/videos/*.*')
-    .pipe(gulp.dest('./dist/'+lang+'/videos/'))
+    .pipe(gulp.dest('./dist/videos/'))
 })
 
 gulp.task('iconify', function() {
-  var lang = gutil.env.lang || defaultLang
   return iconify({
     src: './src/icons/*.svg',
-    cssOutput: './dist/'+lang+'/css/',
-    pngOutput: './dist/'+lang+'/images/icons/',
+    cssOutput: './dist/css/',
+    pngOutput: './dist/images/icons/',
     scssOutput: './dist/scss/'
   })
 })
 
 gulp.task('js', function(){
-  var lang = gutil.env.lang || defaultLang
   return gulp.src('./src/js/index.js')
     .pipe(webpack({ 
       output : { filename : 'index.js' },
@@ -85,13 +85,13 @@ gulp.task('js', function(){
       }
     }))
     .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
-    .pipe(gulp.dest('./dist/'+lang+'/js/'))
+    .pipe(gulp.dest('./dist/js/'))
 })
 
 gulp.task('browser-sync', function() {
-  var lang = gutil.env.lang || defaultLang
+  var lang = 'ru';
   browserSync({
-    server: { baseDir: './dist/'+lang+'/' },
+    server: { baseDir: './dist/' },
     port: 7200,
     open: false
   })
